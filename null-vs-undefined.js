dbed332d853f789991997e6d0c136dc8f5b423e6
IbrahimOhid <ibrahimohid77@gmail.com>
2021-03-10 21:17:55 +0600

    null-vs-undifined

---
+++ null-vs-undefined.js
@@ -0,0 +1,19 @@
+let bird;
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+
+
+const person = {name: "ibrahim", phone: 03333838};
+console.log(person. address);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 33, 44];
+console.log(ages[11]);
